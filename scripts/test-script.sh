f1c58e01341b85ca8096daf746eab50d58c20c22
arsha-prasannan <97940074+arsha-prasannan@users.noreply.github.com>
2024-09-27 10:58:02 +0530

    script

--- scripts/test-script.sh
+++ scripts/test-script.sh
@@ -3,7 +3,7 @@
 
 # Create a file in the home directory
 output_file="$HOME/github_details.txt"
-
+set -x
 # Get details from GitHub Actions environment variables
 echo "Branch Name: $GITHUB_REF_NAME" > $output_file
 echo "Repository: $GITHUB_REPOSITORY" >> $output_file
@@ -14,5 +14,8 @@ echo "Actor: $GITHUB_ACTOR" >> $output_file
 echo "Event: $GITHUB_EVENT_NAME" >> $output_file
 
 echo "Details written to $output_file"
-
+cat $output_file
+ sudo apt-get install cowsay -y
+ cowsay -f dragon " Running a new cosay command .. This is dragon!!!!!!" > dragon.txt
+ cat dragon.txt
 # End of script
